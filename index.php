2ca46ee1412a73d2a747748335370647d594ee87
martynishin <martynishins@gmail.com>
2020-12-28 18:07:28 +0200

    Modified Travis CI config

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
 <?php
 
-echo "<h1>Hello PHP 8</h1>";
+//echo "<h1>Hello PHP 8</h1>";
+
+$servername = "mysql";
+$username = "docker";
+$password = "password";
+$dbname = "docker";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table users created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$q = "INSERT INTO users (name, email)
+VALUES ('Andrew', 'test@gmail.com')";
+
+if ($conn->query($q) === TRUE) {
+    echo "Inserted successfully";
+} else {
+    echo "Error inserting: " . $conn->error;
+}
+
+$conn->close();
